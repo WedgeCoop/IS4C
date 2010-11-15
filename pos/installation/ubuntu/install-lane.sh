5dcdec13db1c4288b193c00131a70cbc6d6fd217
maxolasersquad <maxolasersquad@gmail.com>
2010-11-14 23:26:18 -0500

    Fixed problems in the Ubuntu lane installation script.

--- pos/installation/ubuntu/install-lane.sh
+++ pos/installation/ubuntu/install-lane.sh
@@ -9,6 +9,8 @@ echo "mysql-server mysql-server/root_password_again select $ROOT_PASSWORD" | deb
 echo "python-mysqldb mysql-server/root_password select $ROOT_PASSWORD" | debconf-set-selections
 echo "python-mysqldb mysql-server/root_password_again select $ROOT_PASSWORD" | debconf-set-selections
 aptitude install -y mysql-server apache2 php5 libapache2-mod-php5 python-mysqldb php5-mysql
+cp -r ../../../pos /pos
+chgrp -R www-data /pos
 python /pos/installation/install_lane.py
 
 /pos/installation/ubuntu/ssd_server.sh
